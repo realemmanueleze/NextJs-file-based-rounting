e105648357730a489dfb27198c919eff77f62ca7
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-11-01 11:12:23 +0100

    feat: dynamic path in action

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {useRouter} from 'next/router'
 
 function Portfolio() {
+  const route = useRouter()
+  console.log(route)
+  const name = route.query.id
   return (
-    <div><h1>This is Joshua's Portfolio</h1></div>
+    <div><h1>This is {name} Portfolio</h1></div>
   )
 }
-
 export default Portfolio
