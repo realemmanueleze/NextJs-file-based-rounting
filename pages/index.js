74532038e88fcec1225016f51a49f2a751aa9a54
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2022-11-02 12:56:58 +0100

    feat: added programatic navigation with nextJs <Link href=> component. This is similar to react-router Link component, however react-router takes to='' attribute whereas Next Link takes href= attribute

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 function HomePage() {
   return (
-    <div><h1>Home Page</h1></div>
-  )
+    <div>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Client</Link>
+        </li>
+        <li>
+          <Link href="/clients/1">Specific</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
